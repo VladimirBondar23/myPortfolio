bc41f8c94fdcb14eb8bba2e94336fc597d777b1f
VladimirBondar23 <vdecbel@gmail.com>
2026-03-05 11:53:24 +0200

    vite and tailwind initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
